4435e71fc32ce6dca64c9a1b7f85f2cc1bd104f0
raeolu <raeolu>
2023-01-24 19:30:07 +0100

    Use the right relative path now

---
+++ packmemes.py
@@ -0,0 +1,16 @@
+import os
+import glob
+
+template = open("memetemplate.html", "r")
+
+template_text = template.read()
+
+exts = ['png', 'jpg', 'jpeg', 'gif']
+
+for ext in exts:
+    for filename in glob.iglob(os.getcwd() + "**/**/*." + ext, recursive=True):
+        meme_html = open(filename + ".html", "w")
+        meme_html.write(template_text.replace("MEME_SRC", filename[len(os.getcwd()):]))
+        meme_html.close()
+
+template.close()
